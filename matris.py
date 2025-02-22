bc45eceffee4cacf0d863d21b9198320d386236a
Omar <omar.aldabbagh03@gmail.com>
2025-02-22 17:09:32 +0100

    some updates

--- matris.py
+++ matris.py
@@ -34,16 +34,47 @@ def interpolate_g(g_sparse, Z_sparse, Z_full):
     Z_sparse: The corresponding pixel values where g_sparse was computed (M//10000 values).
     Z_full: The full set of pixel values (M values) for which we need to interpolate g(Z).
     """
+    N = len(Z_sparse)
+    g_new = g_sparse.reshape(N, len(g_sparse) // N)
+    print(np.shape(g_new))
+    print(np.shape(Z_sparse))
     # Create an interpolation function
-    interpolator = interp1d(Z_sparse, g_sparse, kind='linear', fill_value="extrapolate")
-    
-    # Interpolate g(Z) for all pixels in Z_full
-    g_full = interpolator(Z_full)
+    g_full = []
+    for i in range(N):
+        interpolator = interp1d(Z_sparse[i], g_new[i], kind='linear', fill_value="extrapolate")
+        
+        # Interpolate g(Z) for all pixels in Z_full
+        g = interpolator(Z_full)
+        g_full.append(g)
     
     return g_full
 
 
-def compute_matrices(images, exposure_times):
+def create_g_function(g_values, Zprim):
+    """
+    Creates an interpolation function g that can be applied to a general input Z.
+    
+    Parameters:
+    g_values (np.array): The approximated g function values.
+    Zprim (list of np.array): The downsampled pixel intensity values used for approximation.
+    
+    Returns:
+    function: An interpolated function g(Z) that can be applied to general Z values.
+    """
+    
+    # Flatten Zprim to get all sampled intensity values
+    Z_flat = np.concatenate([img.flatten() for img in Zprim])
+    
+    # Ensure uniqueness by sorting and removing duplicates (monotonic increase for interpolation)
+    unique_Z = np.unique(Z_flat)
+    unique_g = g_values[:len(unique_Z)]  # Match corresponding g values
+    
+    # Create an interpolation function
+    g_interpolated = interp1d(unique_Z, unique_g, kind='linear', fill_value='extrapolate')
+    
+    return g_interpolated
+
+def approximate_g(images, exposure_times):
     N = len(images)  # Antalet bilder
     M = images[0].shape[0] * images[0].shape[1] # Antalet pixlar i varje bild (antar att bilderna har samma storlek)
 
@@ -57,8 +88,12 @@ def compute_matrices(images, exposure_times):
 
     # Vektorn b som är ln(exponeringstider)
     ln_delta_t = np.log(exposure_times)
+
+    x = spla.lsqr(A, ln_delta_t)[0]  
+
+    g = x[M:]
     
-    return A, ln_delta_t
+    return g
 
 
 def downsample_image(image, r):
@@ -96,15 +131,29 @@ def merge_hdr(images, exposure_times, g):
     log_exposures = np.log(exposure_times)
     
     # Tillämpa Debevecs HDR alogritm
-    numerator = np.sum(weights * (g - log_exposures.reshape(1,1,1,-1)), axis=-1)
+    numerator = np.sum(weights * (g(images) - log_exposures.reshape(1,1,1,-1)), axis=-1)
     denominator = np.sum(weights, axis=-1)
     hdr= np.exp(numerator / (denominator + epsilon)) # exponering
     return hdr
 
-def least_squares_solution(A, b):
-    x = spla.lsqr(A, b)[0]  
-    return x
+def tone_map(hdr_image):
+    # Standard Rec. 709 för att beräkna luminans utifrån RGB värderna
+    luminance = 0.2126 * hdr_image[..., 0] + 0.7152 * hdr_image[..., 1] + 0.0722 * hdr_image[..., 2]
 
+    # Beräkna log-medel luminans
+    log_average_lum = np.exp(np.mean(np.log(luminance + epsilon)))
+    normal_luminance = luminance / log_average_lum
+
+    # Tillämpa Reinhard tone-mapping
+    tone_mapped_luminance = normal_luminance / (1 + normal_luminance)
+
+    # Använd tone-mappad luminansen för att skala ner färg kanalerna
+    ldr_image = (hdr_image / (luminance[..., None] + epsilon)) * tone_mapped_luminance[..., None]
+
+    ldr_image = np.clip(ldr_image, 0, 1)  # Klipp bort värderna utanför intervallet [0,1]
+    ldr_image = (ldr_image * 255).astype(np.uint8) # Konvertera till 8-bitar
+
+    return ldr_image
 
 def save_image(filename, image):
     imageio.imwrite(filename, image)
@@ -116,26 +165,22 @@ if __name__ == "__main__":
 
     images = np.array(images)
 
-    #Zprim = images[:100:,:100:,:, :]
-
     Zprim = [downsample_image(image, 100) for image in images]
 
-    # Skapa den stora matrisen A och vektorn b
-    A, ln_delta_t = compute_matrices(Zprim, exposure_times)
-
-    # Lös systemet genom minsta kvadratmetoden
-    x = least_squares_solution(A, ln_delta_t)
+    # approximera g
+    g_z_values = approximate_g(Zprim, exposure_times)
 
     M = Zprim[0].shape[0] * Zprim[0].shape[1]
 
-    # Dela upp resultatet i de olika delarna: e och g(Z)
-    e_values = x[:M]  # e är de första N värdena
-    g_z_values = x[M:]  # g(Z) är de resterande värdena
-
     #g_full = interpolate_g(g_z_values, Zprim, images)
 
-    save_image("downsampled.JPG", Zprim[0])
+    #save_image("downsampled1.JPG", Zprim[0])
+    #save_image("downsampled2.JPG", Zprim[1])
+    #save_image("downsampled3.JPG", Zprim[2])
 
-    print(M)
-    print("Vektorn e:", e_values)
-    print("Vektorn g(Z):", np.shape(Zprim))
+    g = create_g_function(g_z_values, Zprim)
+
+    #hdr_image = merge_hdr(images, exposure_times, g)
+    #ldr_image = tone_map(hdr_image)
+    #save_image("hdr_image.JPG", ldr_image)
+    
